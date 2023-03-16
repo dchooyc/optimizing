82edec2401755bfc0c76eb31f3b41462bc5bdc2d
dchooyc <dchooyc@gmail.com>
2023-03-17 03:44:56 +0800

    Tidy up testing with data

--- consistent-hashing/consistent-hashing.py
+++ consistent-hashing/consistent-hashing.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Server:
     def __init__(self, id):
         self.id = id
@@ -11,6 +13,26 @@ class ConsistentHashing:
         self.virtuals = virtuals
         self.assignedValues = []
         self.assignedToServer = {}
+
+    
+    def displayServers(self):
+        print("\n--- Displaying current servers ---")
+
+        for server in self.servers:
+            print("\n" + server.id)
+            print(server.data)
+
+    
+    def displayServersUsage(self):
+        print("\n--- Current servers usage ---")
+
+        for server in self.servers:
+            print("Data usage of " + server.id + ": " + str(len(server.data)))
+
+
+    def addServers(self, serverIds):
+        for serverId in serverIds:
+            self.addServer(serverId)
     
 
     def addServer(self, serverId):
@@ -65,77 +87,74 @@ class ConsistentHashing:
         return mig
     
 
+    def addDataToServers(self, data):
+        for key in data:
+            hashed = hash(key)
+            assignedValue = hashed % 360
+            targetServer = self.findServer(assignedValue)
+            self.addDataToServer(self.servers[targetServer], { key: data[key]})
+    
+
     def addDataToServer(self, server, data):
         for key in data:
             server.data[key] = data[key]
 
 
 
+class TestData:
+    def genServers(self, count):
+        direction = ["North", "South", "East", "West"]
+        countries = ["Singapore", "Australia", "France", "Canada", "China", "Chile", "Nepal", "Uruguay", "Cameroon"]
+
+        servers = [""] * count
+
+        for i in range(count):
+            servers[i] = direction[randint(0,3)] + "-" + countries[randint(0,8)] + "-" + str(randint(1,9))
+
+        return servers
+    
+
+    def genData(self, count):
+        colors = [
+            "red", "blue", "green", "yellow", "orange",
+            "purple", "pink", "brown", "gray", "cyan"
+        ]
+        fruits = [
+            "apple", "banana", "orange", "grape", "pineapple",
+            "mango", "kiwi", "strawberry", "blueberry", "raspberry",
+            "papaya", "watermelon", "cantaloupe", "peach", "nectarine",
+            "plum", "apricot", "cherimoya", "pomegranate", "coconut"
+        ]
+
+        data = {}
+
+        for _ in range(count):
+            data["Fruit" + "-" + str(randint(100_000,999_999))] = colors[randint(0,9)] + "-" + fruits[randint(0,19)]
+        
+        return data
+
+
+
 def main():
-    servers = ["Singapore", "Australia", "France"]
+    td = TestData()
+    servers = td.genServers(3)
     consistent_hashing = ConsistentHashing()
-
-    for server in servers:
-        consistent_hashing.addServer(server)
-    
-    data = ({"Fruit1": "Apple", "Fruit2": "Pear",
-             "Fruit3": "Orange", "Fruit4": "Pineapple",
-             "Fruit5": "Wintermelon", "Fruit6": "Starfruit",
-             "Fruit7": "Banana", "Fruit8": "Grape",
-             "Fruit9": "Strawberry", "Fruit10": "Peach",
-             "Fruit11": "Orange", "Fruit12": "Pineapple",
-             "Fruit13": "Wintermelon", "Fruit14": "Starfruit",
-             "Fruit15": "Banana", "Fruit16": "Grape",
-             "Fruit17": "Strawberry", "Fruit18": "Peach",
-             "Fruit19": "Orange", "Fruit20": "Pineapple"
-             })
-    
-    for key in data:
-        hashed = hash(key)
-        assignedValue = hashed % 360
-        targetServer = consistent_hashing.findServer(assignedValue)
-        consistent_hashing.addDataToServer(consistent_hashing.servers[targetServer], { key: data[key]})
-
-    print("\n--- Displaying original set of servers ---")
-
-    for server in consistent_hashing.servers:
-        print("\n" + server.id)
-        print(server.data)
-
-    newServers = ["Canada", "China", "Chile"]
-
-    for server in newServers:
-        consistent_hashing.addServer(server)
-
-    print("\n--- Displaying servers after adding more servers ---")
-
-    for server in consistent_hashing.servers:
-        print("\n" + server.id)
-        print(server.data)
-
-    newData = ({"Fruit21": "Wintermelon", "Fruit22": "Starfruit",
-                "Fruit23": "Banana", "Fruit24": "Grape",
-                "Fruit25": "Strawberry", "Fruit26": "Peach",
-                "Fruit27": "Orange", "Fruit28": "Pineapple",
-                "Fruit29": "Wintermelon", "Fruit30": "Starfruit",
-                "Fruit31": "Banana", "Fruit32": "Grape",
-                "Fruit33": "Strawberry", "Fruit34": "Peach",
-                "Fruit35": "Orange", "Fruit36": "Pineapple",
-                "Fruit37": "Wintermelon", "Fruit38": "Starfruit",
-                "Fruit39": "Banana", "Fruit40": "Grape",
-                "Fruit41": "Strawberry", "Fruit42": "Peach"})
-    
-    for key in newData:
-        hashed = hash(key)
-        assignedValue = hashed % 360
-        targetServer = consistent_hashing.findServer(assignedValue)
-        consistent_hashing.addDataToServer(consistent_hashing.servers[targetServer], { key: newData[key]})
-
-    print("\n--- Displaying servers after adding more data ---")
-
-    for server in consistent_hashing.servers:
-        print("\n" + server.id)
-        print(server.data)
+    consistent_hashing.addServers(servers)
+    data = td.genData(10)
+    consistent_hashing.addDataToServers(data)
+    consistent_hashing.displayServers()
+    newServers = td.genServers(3)
+    consistent_hashing.addServers(newServers)
+    consistent_hashing.displayServers()
+    newData = td.genData(50)
+    consistent_hashing.addDataToServers(newData)
+    consistent_hashing.displayServers()
+    newData2 = td.genData(100_000)
+    consistent_hashing.addDataToServers(newData2)
+    consistent_hashing.displayServersUsage()
+    newServers2 = td.genServers(6)
+    consistent_hashing.addServers(newServers2)
+    consistent_hashing.displayServersUsage()
     
     return
 
