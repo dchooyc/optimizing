839fe7424557ead0554600578f3d5b0935186f60
dchooyc <dchooyc@gmail.com>
2023-03-16 23:34:45 +0800

    Add migration data functionality

--- consistent-hashing/consistent-hashing.py
+++ consistent-hashing/consistent-hashing.py
@@ -4,6 +4,7 @@ class Server:
         self.data = {}
 
 
+
 class ConsistentHashing:
     def __init__(self, virtuals = 3):
         self.servers = []
@@ -11,20 +12,65 @@ class ConsistentHashing:
         self.assignedValues = []
         self.assignedToServer = {}
     
+
     def addServer(self, serverId):
         newServer = Server(serverId)
         self.servers.append(newServer)
 
         for v in range(1, self.virtuals + 1):
             virtualId = serverId + "." + str(v)
-            assignedValue = hash(virtualId) % 360
+            hashed = hash(virtualId)
+            assignedValue = hashed % 360
             self.assignedValues.append(assignedValue)
             self.assignedToServer[assignedValue] = len(self.servers) - 1
+
+            if len(self.servers) > 0:
+                predecessor = self.findServer(assignedValue)
+                print(predecessor)
+                migrationData = self.getMigrationData(self.servers[predecessor], assignedValue)
+                self.addDataToServer(self.servers[len(self.servers) - 1], migrationData)        
         
         self.assignedValues.sort()
-        print(self.servers)
+        print("These are the assignedValues")
         print(self.assignedValues)
+        print("These are the assigned values to server")
         print(self.assignedToServer)
+        
+
+    def findServer(self, value):
+        lo, hi = 0, len(self.assignedValues)
+
+        while lo < hi:
+            mid = lo + ((hi - lo) >> 1)
+
+            if self.assignedValues[mid] < value:
+                lo = mid + 1
+            else:
+                hi = mid
+        
+        assignedValue = self.assignedValues[lo % len(self.assignedValues)]
+        
+        return self.assignedToServer[assignedValue]
+    
+
+    def getMigrationData(self, server, target):
+        mig = {}
+
+        for key in server.data:
+            hashed = hash(key)
+            assignedValue = hashed % 360
+
+            if assignedValue < target:
+                mig[key] = server.data[key]
+                del server.data[key]
+        
+        return mig
+    
+
+    def addDataToServer(self, server, data):
+        for key in data:
+            server.data[key] = data[key]
+
 
 
 def main():
