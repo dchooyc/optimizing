411e1bd4416f30b674ff18e3e94eaaa57615dbaf
dchooyc <dchooyc@gmail.com>
2023-03-17 00:22:38 +0800

    Add test to show redistribution of data

--- consistent-hashing/consistent-hashing.py
+++ consistent-hashing/consistent-hashing.py
@@ -26,15 +26,10 @@ class ConsistentHashing:
 
             if len(self.servers) > 0:
                 predecessor = self.findServer(assignedValue)
-                print(predecessor)
                 migrationData = self.getMigrationData(self.servers[predecessor], assignedValue)
                 self.addDataToServer(self.servers[len(self.servers) - 1], migrationData)        
         
         self.assignedValues.sort()
-        print("These are the assignedValues")
-        print(self.assignedValues)
-        print("These are the assigned values to server")
-        print(self.assignedToServer)
         
 
     def findServer(self, value):
@@ -54,7 +49,7 @@ class ConsistentHashing:
     
 
     def getMigrationData(self, server, target):
-        mig = {}
+        mig, removedKeys = {}, []
 
         for key in server.data:
             hashed = hash(key)
@@ -62,7 +57,10 @@ class ConsistentHashing:
 
             if assignedValue < target:
                 mig[key] = server.data[key]
-                del server.data[key]
+                removedKeys.append(key)
+        
+        for key in removedKeys:
+            del server.data[key]
         
         return mig
     
@@ -79,6 +77,65 @@ def main():
 
     for server in servers:
         consistent_hashing.addServer(server)
+    
+    data = ({"Fruit1": "Apple", "Fruit2": "Pear",
+             "Fruit3": "Orange", "Fruit4": "Pineapple",
+             "Fruit5": "Wintermelon", "Fruit6": "Starfruit",
+             "Fruit7": "Banana", "Fruit8": "Grape",
+             "Fruit9": "Strawberry", "Fruit10": "Peach",
+             "Fruit11": "Orange", "Fruit12": "Pineapple",
+             "Fruit13": "Wintermelon", "Fruit14": "Starfruit",
+             "Fruit15": "Banana", "Fruit16": "Grape",
+             "Fruit17": "Strawberry", "Fruit18": "Peach",
+             "Fruit19": "Orange", "Fruit20": "Pineapple"
+             })
+    
+    for key in data:
+        hashed = hash(key)
+        assignedValue = hashed % 360
+        targetServer = consistent_hashing.findServer(assignedValue)
+        consistent_hashing.addDataToServer(consistent_hashing.servers[targetServer], { key: data[key]})
+
+    print("\n--- Displaying original set of servers ---")
+
+    for server in consistent_hashing.servers:
+        print("\n" + server.id)
+        print(server.data)
+
+    newServers = ["Canada", "China", "Chile"]
+
+    for server in newServers:
+        consistent_hashing.addServer(server)
+
+    print("\n--- Displaying servers after adding more servers ---")
+
+    for server in consistent_hashing.servers:
+        print("\n" + server.id)
+        print(server.data)
+
+    newData = ({"Fruit21": "Wintermelon", "Fruit22": "Starfruit",
+                "Fruit23": "Banana", "Fruit24": "Grape",
+                "Fruit25": "Strawberry", "Fruit26": "Peach",
+                "Fruit27": "Orange", "Fruit28": "Pineapple",
+                "Fruit29": "Wintermelon", "Fruit30": "Starfruit",
+                "Fruit31": "Banana", "Fruit32": "Grape",
+                "Fruit33": "Strawberry", "Fruit34": "Peach",
+                "Fruit35": "Orange", "Fruit36": "Pineapple",
+                "Fruit37": "Wintermelon", "Fruit38": "Starfruit",
+                "Fruit39": "Banana", "Fruit40": "Grape",
+                "Fruit41": "Strawberry", "Fruit42": "Peach"})
+    
+    for key in newData:
+        hashed = hash(key)
+        assignedValue = hashed % 360
+        targetServer = consistent_hashing.findServer(assignedValue)
+        consistent_hashing.addDataToServer(consistent_hashing.servers[targetServer], { key: newData[key]})
+
+    print("\n--- Displaying servers after adding more data ---")
+
+    for server in consistent_hashing.servers:
+        print("\n" + server.id)
+        print(server.data)
     
     return
 
